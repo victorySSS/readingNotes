f78ea1084e6f3f924801f2ad41251d50e6f4807b
Ludics <me@ludics.cn>
2017-12-12 01:30:17 +0800

    First add communicate module

---
+++ diary/app/src/main/java/com/crowd/diary/internet/Communicate.java
@@ -0,0 +1,19 @@
+package com.crowd.diary.internet;
+
+/**
+ * Created by leon on 17-12-12.
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Communicate {
+    public void login(String username, String password) throws JSONException{
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("username", username);
+        jsonObj.put("password", password);
+
+    }
+}
