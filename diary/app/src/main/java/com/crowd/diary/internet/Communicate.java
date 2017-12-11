7cab301de0a1e8b827911dfd699d6c8736a28e06
Ludics <me@ludics.cn>
2017-12-12 01:48:30 +0800

    Modify signup

--- diary/app/src/main/java/com/crowd/diary/internet/Communicate.java
+++ diary/app/src/main/java/com/crowd/diary/internet/Communicate.java
@@ -19,6 +19,8 @@ public class Communicate {
 
 
     public void signup(String username, String password) throws JSONException {
-
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("username", username);
+        jsonObj.put("password", password);
     }
 }
