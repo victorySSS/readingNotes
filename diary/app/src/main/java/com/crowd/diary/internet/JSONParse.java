59cf38c4ed08959b3a6a2076fabdca6103412e46
Ludics <me@ludics.cn>
2017-12-13 15:54:52 +0800

    Add new classes

---
+++ diary/app/src/main/java/com/crowd/diary/internet/JSONParse.java
@@ -0,0 +1,9 @@
+package com.crowd.diary.internet;
+
+/**
+ * Created by leon on 17-12-13.
+ */
+
+public class JSONParse {
+
+}
---
+++ diary/app/src/main/java/com/crowd/diary/internet/JSONStruct.java
@@ -0,0 +1,8 @@
+package com.crowd.diary.internet;
+
+/**
+ * Created by leon on 17-12-13.
+ */
+
+public class JSONStruct {
+}
