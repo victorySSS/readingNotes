fae1a1f002760d6c18439ce738e6027a9b12287c
Spring Zhao <740161577@qq.com>
2017-12-15 00:08:43 +0800

    Server 多线程
    利用线程池，支持多用户

--- Server/Server.java
+++ Server/Server.java
@@ -5,30 +5,127 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.PrintWriter;
+
  
 public class Server {
-   public static void main(String[] args) {
-      try {
-         ServerSocket ss = new ServerSocket(8888);
-         System.out.println("opening the Server....");
-         Socket s = ss.accept();
-         System.out.println("Client:"+s.getInetAddress().getLocalHost()+"have connected to the Server");
-         
-         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-         //read the message from the client
-         String UserName = br.readLine();
-         String PassWord = br.readLine();
+    
+    private static final int PORT = 8888;
+    private List<Socket> mList = new ArrayList<Socket>();
+    private ServerSocket server = null;
+    private ExecutorService myExecutorService = null;
+    
+    
+    public static void main(String[] args) {
+        new Server();
+    }
 
-         System.out.println("Name:"+UserName);
-         System.out.println("PassWord:"+PassWord);
+    public Server()
+    {
+        try
+        {
+            server = new ServerSocket(PORT);
+            
+            myExecutorService = Executors.newCachedThreadPool();
+            System.out.println("Server Is Running...\n");
+            Socket client = null;
+            while(true)
+            {
+                client = server.accept();
+                mList.add(client);
+                myExecutorService.execute(new Service(client));
+            }
+            
+        }catch(Exception e){e.printStackTrace();}
+    }
+    
 
 
 
-         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-         bw.write("OK\n");
-         bw.flush();
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+
+    //------------------------------------------------------------
+    class Service implements Runnable
+    {
+        private Socket socket;
+        private BufferedReader in = null;
+        private String msg = "";
+        
+        public Service(Socket socket) {
+            this.socket = socket;
+            try
+            {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 msg = "User:" +this.socket.getInetAddress() + "~join the APP "  
+                            +"Online nubmers:" +mList.size();  
+                System.out.println(msg);
+                //this.sendmsg();
+            }catch(IOException e){e.printStackTrace();}
+        }
+        
+        
+        
+        @Override
+        public void run() {
+            try{
+                while(true)
+                {
+                    if((msg = in.readLine()) != null)
+                    {
+                        if(msg.equals("bye"))
+                        {
+                            System.out.println("~~~~~~~~~~~~");
+                            mList.remove(socket);
+                            in.close();
+                            msg = "User:" + socket.getInetAddress()  
+                                    + "quit:" +"Online nums"+mList.size();  
+                            socket.close();  
+                            //this.sendmsg();  
+                            break;
+                        }
+                        else{
+                            if(msg.equals("zxcv")){
+                                msg="OK\n";
+                                this.sendmsg();
+                            }
+                            else{
+                            msg = socket.getInetAddress() + "   say:" + msg;  
+                            //this.sendmsg(); 
+                            System.out.println(msg);
+                            }
+                        }
+                    }
+
+                    
+                }
+            }catch(Exception e){e.printStackTrace();}
+        }
+        
+        //send message to client
+        public void sendmsg()
+        {
+            
+                PrintWriter pout = null;  
+                try {  
+                    pout = new PrintWriter(new BufferedWriter(  
+                            new OutputStreamWriter(socket.getOutputStream(),"UTF-8")),true);  
+                    pout.println(msg);  
+                    pout.flush();
+                }catch (IOException e) {e.printStackTrace();}  
+            
+        }
+        
+    }
 }
+   
