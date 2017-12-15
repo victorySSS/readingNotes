829ae265755185bbc38062743a2f328f963649f4
Wang Shengli <205514167@qq.com>
2017-12-15 20:50:29 +0800

    Modify Communicacte class

--- diary/app/src/main/java/com/crowd/diary/internet/Communicate.java
+++ diary/app/src/main/java/com/crowd/diary/internet/Communicate.java
@@ -13,55 +13,42 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Communicate {
-    String httpUrl = null;
-    public void login(String username, String password) throws JSONException{
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("username", username);
-        jsonObj.put("password", password);
-        Communicate comm = new Communicate();
-        try {
-            String re = comm.sendPost(httpUrl, jsonObj.toString());
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+    String myUrl = "http://www.ludics.cn/app/";
 
-    }
-
-
-    public void signup(String username, String password) throws JSONException {
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("username", username);
-        jsonObj.put("password", password);
-        Communicate comm = new Communicate();
-        try {
-            String re = comm.sendPost(httpUrl, jsonObj.toString());
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-    private String sendPost(String url,String Params)throws IOException{
+    /**
+     * @作用 注册账号
+     * @param username
+     * @param password
+     * @return "Username existed, register failed."
+     * @return "Register success."
+     * @throws IOException
+     */
+    public String registerToServer(String username,String password) throws IOException{
         OutputStreamWriter out = null;
         BufferedReader reader = null;
         String response="";
         try {
             URL httpUrl = null; //HTTP URL类 用这个类来创建连接
             //创建URL
-            httpUrl = new URL(url);
+            httpUrl = new URL(myUrl+"signup.php");
             //建立连接
             HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
             conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("Charset", "utf-8");
             conn.setUseCaches(false);//设置不要缓存
             conn.setInstanceFollowRedirects(true);
             conn.setDoOutput(true);
             conn.setDoInput(true);
+            conn.setConnectTimeout(3000);
+            String data = "&userName=" + username + "&userPassword=" + password;
+            conn.setRequestProperty("Content-Length",String.valueOf(data.getBytes().length));
             conn.connect();
             //POST请求
-            out = new OutputStreamWriter(
-                    conn.getOutputStream());
-            out.write(Params);
-            out.flush();
+            OutputStream os = conn.getOutputStream();
+            os.write(data.getBytes());
+            os.flush();
             //读取响应
             reader = new BufferedReader(new InputStreamReader(
                     conn.getInputStream()));
@@ -76,7 +63,77 @@ public class Communicate {
             // System.out.println(response.toString());
             // log.info(response.toString());
         } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
+            // System.out.println("发送 POST 请求出现异常！"+e);
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(reader!=null){
+                    reader.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+
+        return response;
+    }
+    /**
+     * @作用 登录
+     * @param username
+     * @param password
+     * @return "Login success."
+     * @return "Password error."
+     * @return "User not exist."
+     * @throws IOException
+     */
+    public String loginToServer(String username, String password) throws IOException{
+        OutputStreamWriter out = null;
+        BufferedReader reader = null;
+        String response="";
+        try {
+
+            URL httpUrl = null; //HTTP URL类 用这个类来创建连接
+            //创建URL
+            httpUrl = new URL(myUrl+"user_login.php");
+            //建立连接
+            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("Charset", "utf-8");
+            conn.setUseCaches(false);//设置不要缓存
+            conn.setInstanceFollowRedirects(true);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setConnectTimeout(3000);
+            String data = "&userName=" + username + "&userPassword=" + password;
+            conn.setRequestProperty("Content-Length",String.valueOf(data.getBytes().length));
+            conn.connect();
+            //POST请求
+            OutputStream os = conn.getOutputStream();
+            os.write(data.getBytes());
+            os.flush();
+            //读取响应
+            reader = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            String lines;
+            while ((lines = reader.readLine()) != null) {
+                lines = new String(lines.getBytes(), "utf-8");
+                response+=lines;
+            }
+            reader.close();
+            // 断开连接
+            conn.disconnect();
+            // System.out.println(response.toString());
+            // log.info(response.toString());
+        } catch (Exception e) {
+            // System.out.println("发送 POST 请求出现异常！"+e);
             e.printStackTrace();
         }
         //使用finally块来关闭输出流、输入流
